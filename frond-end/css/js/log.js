9dc4ac7a7eab386a5569e9f8e837099fdb31454a
Maria Celeste Quiroga Hamoud <139657210+Maria-Quiroga@users.noreply.github.com>
2024-04-01 22:08:18 -0300

    Add files via upload

---
+++ frond-end/css/js/log.js
@@ -0,0 +1,10 @@
+
+let login = [];
+
+const btnIngresar = document.querySelector('#boton-ingresar');
+
+function login() {
+    if (Ingresar.length == 0) {
+      alert("No hay productos en la lista");
+      return;
+    }
